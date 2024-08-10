f7f7e0f9312de36aa4541514407dfa12d92b39d1
Charlotte Lee <charlotteuxd@gmail.com>
2024-08-10 13:17:04 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './dictionary-app-logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={logo}
+          className="App-logo img-fluid"
+          alt="dictionary-logo"
+        ></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
