49279afd14fccce0192982fb7906b95d8fb9aa72
Charlotte Lee <charlotteuxd@gmail.com>
2024-08-17 11:02:38 +0800

    App.js Bug fixes
    Added rel="noreferrer" to anchor tag

--- src/App.js
+++ src/App.js
@@ -17,17 +17,26 @@ function App() {
       </main>
       <footer className="App-footer">
         This project is coded by{' '}
-        <a href="https://www.linkedin.com/in/charlottelhm/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/charlottelhm/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Charlotte Lee
         </a>{' '}
         and it is open-sourced on{' '}
-        <a href="https://github.com/charluxd/dictionary-app" target="_blank">
+        <a
+          href="https://github.com/charluxd/dictionary-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           Github
         </a>{' '}
         and hosted on{' '}
         <a
           href="https://main--modern-dictionary-app.netlify.app/"
           target="_blank"
+          rel="noreferrer"
         >
           Netlify
         </a>
